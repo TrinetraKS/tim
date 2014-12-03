2952bf10da67e2fb4913ca79718ed96858c27906
Trinetra K S <trinetra@Trinetras-MacBook-Pro.local>
2014-12-03 08:39:37 +0530

    w

---
+++ tim/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tim
+//
+//  Created by Trinetra K S on 03/12/14.
+//  Copyright (c) 2014 Hummingwave Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tim/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tim
+//
+//  Created by Trinetra K S on 03/12/14.
+//  Copyright (c) 2014 Hummingwave Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
